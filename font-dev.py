02e7fe6a7e5bddcea765cf6c49ba87e0ce421371
Jeme2552 <medeirosjjv@gmail.com>
2020-03-26 16:28:56 -0300

    finalização da estrutura condicional

--- font-dev.py
+++ font-dev.py
@@ -4,6 +4,7 @@ from random import randint
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Precisa de algo mestre?')
@@ -17,11 +18,67 @@ async def on_message(message):
     if message.content.startswith("!test"):
         await message.channel.send("Sim mestre!")
 
-    if message.content.startswith("!d3"):
-        dice = randint(1,3)
+
+        
+     
+    if message.content.startswith("!d4"):
+        dice = randint(1,4)
         if dice == 1:
-            await message.channel.send('Você se fodeu!')
+            await message.channel.send('FALHA CRÍTICA!')
+        elif dice == 4:
+            await message.channel.send('ACERTO CRÍTICO!')    
+        else:
+            await message.channel.send("{}!".format(dice))  
+ 
+ 
+    if message.content.startswith("!d6"):
+        dice = randint(1,6)
+        if dice == 1:
+            await message.channel.send('FALHA CRÍTICA!')
+        elif dice == 6:
+            await message.channel.send('ACERTO CRÍTICO!')    
         else:
             await message.channel.send("{}!".format(dice))  
 
-client.run('#token do bot)
+    if message.content.startswith("!d8"):
+        dice = randint(1,8)
+        if dice == 1:
+            await message.channel.send('FALHA CRÍTICA!')
+        elif dice == 8:
+            await message.channel.send('ACERTO CRÍTICO!')    
+        else:
+            await message.channel.send("{}!".format(dice)) 
+
+    if message.content.startswith("!d10"):
+        dice = randint(1,10)
+        if dice == 1:
+            await message.channel.send('FALHA CRÍTICA!')
+        elif dice == 10:
+            await message.channel.send('ACERTO CRÍTICO!')    
+        else:
+            await message.channel.send("{}!".format(dice)) 
+
+    if message.content.startswith("!d12"):
+        dice = randint(1,12)
+        if dice == 1:
+            await message.channel.send('FALHA CRÍTICA!')
+        elif dice == 12:
+            await message.channel.send('ACERTO CRÍTICO!')    
+        else:
+            await message.channel.send("{}!".format(dice)) 
+
+
+    if message.content.startswith("!d20"):
+        dice = randint(1,20)
+        if dice == 1:
+            await message.channel.send('FALHA CRÍTICA!')
+        elif dice == 20:
+            await message.channel.send('ACERTO CRÍTICO!')    
+        else:
+            await message.channel.send("{}!".format(dice))                 
+
+
+
+
+
+client.run('#bot token)
